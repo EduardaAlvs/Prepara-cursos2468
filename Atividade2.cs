6640bcc03edf756d685a710e95f623d743dfba37
EduardaAlvs <ea8318611@gmail.com>
2023-04-19 11:53:55 -0300

    .

---
+++ Atividade2.cs
@@ -0,0 +1,12 @@
+using System;
+class program{
+    static void Main(){
+        
+        string nome;
+        Console.WriteLine("Qual é o seu nome?");
+        nome = Console.ReadLine();
+        Console.WriteLine("Boas Vindas!" + nome);
+
+
+    }
+}
---
+++ Atividade3.cs
@@ -0,0 +1,15 @@
+using System;
+class program{
+    static void Main(){
+
+        string nome;
+        int salário;
+        
+        Console.WriteLine("Qual o seu nome?");
+        nome = Console.ReadLine();
+        Console.WriteLine("nome");
+        salário = Console.ReadLine();
+        Console.WriteLine(salário);
+
+    }
+}
